b020dd3f3aec8c1e1e7e948425ee8bab9dc441d4
Erin Boeger <sullymustycode@gmail.com>
2014-07-30 23:02:26 -0600

    seedsrails c

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "Creating exercise groups"
+exercise_groups = ["chest", "arms", "back", "core", "legs"]
+exercise_groups.each do |group|
+  exsist = ExerciseGroup.find_by_name(group)
+  if !exsist
+    new_group = ExerciseGroup.create(name: group)
+    new_group.save!
+  end
+end
+
+puts "Creating contact types"
+contact_types = [{name: "client", description: "person who has purchased"}, {name: "prospect", description: "future client"}]
+contact_types.each do |type|
+  exsist = ContactType.find_by_name(type[:name])
+  if !exsist
+    new_type = ContactType.create(type)
+    new_type.save!
+  end
+end
